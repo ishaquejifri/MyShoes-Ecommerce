0582db032b85514b7e1ffd2248fa1c14bd09d9a5
ishaque <ishaque7jifri@gmail.com>
2026-08-13 14:42:51 +0530

    Update Settings.py

--- myshoes/settings.py
+++ myshoes/settings.py
@@ -16,20 +16,20 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR,".env"))
+load_dotenv(os.path.join(BASE_DIR,".env")) 
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#*bky*#8op(5qn+y0bg*sezj%f-33#b-30m-fznsw!b70z%u!x'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
     
     
 
